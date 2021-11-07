8a681996b13add35bb5a88e9819d86b0fd9e9259
SU-I-KA <Bmagdy@mail.com>
2021-11-07 09:07:20 +0300

    styling using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios'
+import { Table, Form, Col, Container, Row, Spinner } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
   const [countries, setCountries] = useState([])
@@ -62,53 +64,88 @@ const App = () => {
 
   const filterBy = ['all', 'capital']
   return (
-    <div className='container'>
-      <input
-        type='text'
-        // value={query}
-        placeholder='Enter a search value....'
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      filter by:{' '}
-      {filterBy &&
-        filterBy.map((item) => (
-          <div key={item}>
-            <input
-              type='radio'
-              name='filterBy'
-              value={item}
-              checked={item === filter}
-              onChange={(e) => setFilter(e.target.value)}
-            />
-            <label>{item}</label>
-          </div>
-        ))}
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Capital</th>
-            <th>Region</th>
-            <th>Flag</th>
-          </tr>
-        </thead>
-        <tbody>
-          {search(countries)?.map?.((country) => {
-            const { name, capital, region, flag } = country
-            return (
-              <tr key={name}>
-                <td>{name}</td>
-                <td>{capital}</td>
-                <td>{region}</td>
-                <td>
-                  <img src={flag} alt={name} width='120px' />
+    <Container className='mt-5'>
+      <Form>
+        <Form.Group>
+          <Form.Control
+            type='text'
+            size='lg'
+            // value={query}
+            placeholder='Enter a search value....'
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </Form.Group>
+        <Row className='mx-auto w-50 p-3'>
+          <Col>
+            <p className='text-muted'>Filter By Category : </p>
+          </Col>
+          <Col>
+            <Row>
+              {filterBy &&
+                filterBy.map((item) => (
+                  <Col key={item}>
+                    <Form.Group>
+                      <Form.Check
+                        type='radio'
+                        name='filterBy'
+                        value={item}
+                        checked={item === filter}
+                        onChange={(e) => setFilter(e.target.value)}
+                      />
+                      <Form.Label className='text-capitalize'>
+                        {item}
+                      </Form.Label>
+                    </Form.Group>
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+        </Row>
+      </Form>
+      {countries.length > 0 ? (
+        <Table striped bordered hover responsive>
+          <thead className='text-center'>
+            <tr>
+              <th className='col-md-5'>Name</th>
+              <th className='col-md-2'>Capital</th>
+              <th className='col-md-2'>Region</th>
+              <th className='col-md-3'>Flag</th>
+            </tr>
+          </thead>
+          {search(countries).length > 0 ? (
+            <tbody className='text-center'>
+              {search(countries)?.map?.((country) => {
+                const { name, capital, region, flag } = country
+                return (
+                  <tr key={name}>
+                    <td className='align-middle'>{name}</td>
+                    <td className='align-middle'>{capital}</td>
+                    <td className='align-middle'>{region}</td>
+                    <td className='align-middle'>
+                      <img src={flag} alt={name} width='120px' />
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          ) : (
+            <tbody>
+              <tr>
+                <td className='text-center col-md-12' colSpan='4'>
+                  No Match
                 </td>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+            </tbody>
+          )}
+        </Table>
+      ) : (
+        <div className='d-flex justify-content-center'>
+          <Spinner animation='border' role='status'>
+            <span className='sr-only'>Loading...</span>
+          </Spinner>
+        </div>
+      )}
+    </Container>
   )
 }
 
