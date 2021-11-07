8ac45ab6772a7c8bba4021e8536d1b625c88f7a9
SU-I-KA <Bmagdy@mail.com>
2021-11-07 07:38:40 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
 
 const App = () => {
-  return <p>search countries</p>
+  const [countries, setCountries] = useState([])
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get('/v2/all')
+      //console.log(data)
+      setCountries(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div className='container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Capital</th>
+            <th>Region</th>
+            <th>Flag</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries?.map?.((country) => {
+            const { name, capital, region, flag } = country
+            return (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{capital}</td>
+                <td>{region}</td>
+                <td>
+                  <img src={flag} alt={name} width='120px' />
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default App
